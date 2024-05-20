39051f87b8cd7b4f598ccd70131417ed8a563beb
wrfmartinez <wrmartinez709@gmail.com>
2024-05-19 23:30:58 -0400

    Add update and delete routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import Item from './models/Item';
+import Item from './models/Item.js';
 
 dotenv.config();
 
@@ -32,6 +32,32 @@ app.get('/items', async (req, res) => {
   try {
     const items = await Item.find();
     res.status(200).json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update
+app.put('/items/:id', async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body);
+    if (!updatedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.status(200).json(updatedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const deletedItem = await Item.findByIdAndDelete(req.params.id);
+    if (!deletedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.status(200).json({ message: 'Item deleted' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
